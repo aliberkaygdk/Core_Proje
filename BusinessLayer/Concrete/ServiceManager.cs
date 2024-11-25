f5cbda7cd530af5c1af51a0e64f1d2d7202eb858
aliberkaygdk <aliberkaygdk@gmail.com>
2024-11-26 01:56:03 +0300

    service and skill

---
+++ BusinessLayer/Concrete/ServiceManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ServiceManager : IGenericService<Service>
+    {
+        IServiceDal service;
+
+        public ServiceManager(IServiceDal service)
+        {
+            this.service = service;
+        }
+
+        public void TAdd(Service t)
+        {
+            service.Insert(t);
+        }
+
+        public void TDelete(Service t)
+        {
+            service.Delete(t);
+        }
+
+        public Service TGetById(int id)
+        {
+            return service.GetByID(id);
+        }
+
+        public List<Service> TGetList()
+        {
+            return service.GetList();
+        }
+
+        public void TUpdate(Service t)
+        {
+            service.Update(t);
+        }
+    }
+}
