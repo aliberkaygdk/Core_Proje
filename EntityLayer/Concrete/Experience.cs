b02806be3ec9e5520d7a3429cd63b14dde4c31f9
aliberkaygdk <aliberkaygdk@gmail.com>
2024-11-27 00:23:39 +0300

    pages added, db updated

--- EntityLayer/Concrete/Experience.cs
+++ EntityLayer/Concrete/Experience.cs
@@ -14,5 +14,6 @@ namespace EntityLayer.Concrete
         public string Name { get; set; }
         public string Date { get; set; }
         public string ImageUrl { get; set; }
+        public string Description { get; set; }
     }
 }
